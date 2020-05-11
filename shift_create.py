38fda5a0312cc3e7549f8cda8ef5e9799d433faa
Naoki Nakamichi <ronnao0516@gmail.com>
2020-05-11 13:58:15 +0900

    tmp

--- shift_create.py
+++ shift_create.py
@@ -9,10 +9,6 @@ from openpyxl.styles import PatternFill
 
 
 class Shift:
-    def __init__(self):
-        pass
-
-class GetExcelData:
     def __init__(self,name):
         self.shift = pd.read_excel(name,sheet_name='Shift',index_col=0)
         self.manage = pd.read_excel(name,sheet_name='Manage',index_col=0)
@@ -20,20 +16,28 @@ class GetExcelData:
         self.member = self.member.T
         self.detail = pd.read_excel(name,sheet_name='Detail',index_col=0)
 
+        print(self.shift)
+
+        # 従業員数と月の日数
+        days, number_employee = self.shift.shape[0], self.shift.shape[1]
+
+        # 日付の配列、従業員の名前の配列
+        date,employee = self.shift.index, self.shift.columns
+
+        # 変数の追加
+        # self.shift['V_need_dif'] = addvars(days)         # 必要人数に達していない時のペナルティ変数
+        # self.shift['V_gender_rate'] = addvars(days)      # 女性が少ない時のペナルティ
+        # self.shift['V_experience'] = addvars(days)       # 新人だけになった時のペナルティ
+        # self.shift['need'] = self.manage.need            # 必要人数
+
+        # 足りない人が入れば終了
+        # shortage = []
+        # for index,r in self.shift[employee].iterrows():
+        #     if sum(r) < int(self.shift.at[index,'need']):
+        #         shortage.append(index)
+        # if shortage:
+        #     raise ValueError(day + '日足りません')
+
+
     def return_data(self):
-
         return self.shift, self.manage, self.member, self.detail
-
-
-class ShiftCheck:
-    def __init__(self):
-        shortage = []
-        for index,r in shift[employee].iterrows():
-            if sum(r) < int(shift.at[index,'need']):
-                shortage.append(index)
-        if shortage:
-            day = ''
-            for i in shortage:
-                day += (str(i) + '日足りません')
-                print(day)
-            sys.exit()
